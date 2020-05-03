15c1b3ecc65dc66d975e2fde0606048e9ab5c16e
김희윤 <dnflwlq3231@naver.com>
2020-05-04 04:20:54 +0900

    UPGRADE UX

--- myPractice/public/javascripts/room.js
+++ myPractice/public/javascripts/room.js
@@ -1,123 +1,162 @@
 'use strict'
+$(document).ready(askName)
 
-/////////////////////////////////////////////////////////////////
-////////////////////////// webRTC PART /////////////////////////
-///////////////////////////////////////////////////////////////
+$('#make').on('click', makeRoom)
 
-let localStream;
-let remoteStream;
-let room = 'corgi';
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-const constraints = {
-    video: true,
-    audio: false
-};
-
-// STUN server config
-const pcConfig = {
-    'iceServers': [
-        { 'urls': 'stun:stun.l.google.com:19302' },
-        { 'urls': 'stun:stun.services.mozilla.com' }
-    ]
-};
-
-// check if the browser supports the WebRTC
-function hasLocalMedia() {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+function askName() {
+    $('#room').hide();
+    $('#displayId').hide();
+    $('#hangout').hide();
+    $('#localVideo').hide();
 }
 
-function recStream(stream, elemid) {
-    let video = document.getElementById(elemid);
+async function makeRoom() {
+    let room = await $('#roomName').val();
+    $('#room').show();
+    $('#displayId').show();
+    $('#hangout').show();
+    $('#localVideo').show();
+    $('#askRoomName').hide();
+    $('#roomName').hide();
+    $('#make').hide();
 
-    video.srcObject = stream;
-    remoteStream = stream;
-}
 
-// get local media
-function getLocalStream(stream) {
-    localStream = stream;
-    recStream(stream, 'localVideo');
-}
+    /////////////////////////////////////////////////////////////////
+    ////////////////////////// webRTC PART /////////////////////////
+    ///////////////////////////////////////////////////////////////
 
-// Handles error by logging a message to the console with the error message.
-function handleLocalMediaStreamError(error) {
-    console.log('navigator.getUserMedia error: ', error);
-}
-
-if (hasLocalMedia()) {
-
-    // enabling video and audio channels
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(getLocalStream)
-        .catch(handleLocalMediaStreamError)
-
-} else {
-    alert('webRTC is not supported!')
-}
-
-/////////////////////////////////////////////////////////////////
-////////////////////////// peerJS PART /////////////////////////
-///////////////////////////////////////////////////////////////
-
-let peer = new Peer(room, { key: 'lwjd5qra8257b9' });
-let conn;
-
-peer.on('open', (id) => {
-    document.getElementById("displayId").innerHTML = peer.id
-    console.log(id)
-})
-
-// try to connect
-function connecting() {
-    let peerId = $('#connId').val();
-    conn = peer.connect(peerId, {label: 'room'});
+    let localStream;
+    let remoteStream;
     
-    // connect 할 때 user 로 보내는 값 (1)
-    conn.on('open', () => {
-        conn.send('room is ready');
-    })
 
-    // connect user 에서 받는 값 (4)
-    conn.on('data', (data) => {
-        if (data === "user is ready") {
-            navigator.getUserMedia(constraints, (stream) => {
-                var call = peer.call(peerId, stream);
-                call.on('stream', (stream) => {
-                    remoteStream = stream;
-                    recStream(remoteStream, 'remoteVideo')
-                })
-            })
-        } else {
-            alert('connecting error 2');
-        }
-    })
-}
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-// listen connecting
-peer.on('connection', (connection) => {
-    console.log(connection)
-    
-    // user 에서 보내는 id 값 받는 곳 (2)
-    connection.on('data', (data) => {
-        console.log('user ' , data)
-        $('#connId').val(data);
-    })
+    const constraints = {
+        video: true,
+        audio: false
+    };
 
-    // 받을 때 되돌려주는 값 (3)
-    connection.on('open', () => {
-        connection.send('room is alive');
-    });
-});
+    // STUN server config
+    const pcConfig = {
+        'iceServers': [
+            { 'urls': 'stun:stun.l.google.com:19302' },
+            { 'urls': 'stun:stun.services.mozilla.com' }
+        ]
+    };
 
-// 전화 대기
-peer.on('call', (call) => {
-    call.answer(localStream);
-    call.on('stream', (stream) => {
+    // check if the browser supports the WebRTC
+    function hasLocalMedia() {
+        return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+    }
+
+    function recStream(stream, elemid) {
+        let video = document.getElementById(elemid);
+
+        video.srcObject = stream;
         remoteStream = stream;
-        recStream(stream, 'remoteVideo')
-    })
-})
+    }
 
-$('#connButton').on('click', connecting)
+    // get local media
+    function getLocalStream(stream) {
+        localStream = stream;
+        recStream(stream, 'localVideo');
+    }
+
+    // Handles error by logging a message to the console with the error message.
+    function handleLocalMediaStreamError(error) {
+        console.log('navigator.getUserMedia error: ', error);
+    }
+
+    if (hasLocalMedia()) {
+
+        // enabling video and audio channels
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(getLocalStream)
+            .catch(handleLocalMediaStreamError)
+
+    } else {
+        alert('webRTC is not supported!')
+    }
+
+    /////////////////////////////////////////////////////////////////
+    ////////////////////////// peerJS PART /////////////////////////
+    ///////////////////////////////////////////////////////////////
+
+    let peer = new Peer(room, { key: 'lwjd5qra8257b9' });
+    let conn;
+    let count;
+    let userId; // 참여 원하는 사람 id 값
+
+    peer.on('open', (id) => {
+        document.getElementById("displayId").innerHTML = peer.id
+    })
+
+    // try to connect
+    function connecting() {
+        // let peerId = $('#connId').val();
+        let peerId = userId;
+        conn = peer.connect(peerId, { label: 'room' });
+
+        // connect 할 때 user 로 보내는 값 (3)
+        conn.on('open', () => {
+            conn.send('room is ready');
+        })
+
+        // connect user 에서 받는 값 (4)
+        conn.on('data', (data) => {
+            if (data === "user is ready") {
+                navigator.getUserMedia(constraints, (stream) => {
+                    var call = peer.call(peerId, stream);
+                    call.on('stream', (stream) => {
+                        remoteStream = stream;
+                        recStream(remoteStream, 'remoteVideo')
+                    })
+                })
+            } else {
+                alert('connecting error 2');
+            }
+        })
+    }
+
+    function shutDown() {
+        peer.destroy();
+    }
+
+    // listen connecting
+    peer.on('connection', (connection) => {
+
+        // user 에서 보내는 id 값 받는 곳 (1)
+        connection.on('data', (data) => {
+            // user id 받음
+            let join = confirm(data + ' 참여하길 원합니다');
+            userId = data;
+            if (join === true) {
+                connecting()
+                count += 1;
+            } else {
+                alert('거절하셨습니당')
+            }
+        })
+
+        // 받을 때 되돌려주는 값 (2)
+        connection.on('open', () => {
+            if (count !== 0) {
+                connection.send({would: '입장하시겠습니까', name: room});
+            } else {
+                connection.send({would: '꺼져', name: null});
+            }
+        });
+    });
+
+    // 전화 대기
+    peer.on('call', (call) => {
+        call.answer(localStream);
+        call.on('stream', (stream) => {
+            remoteStream = stream;
+            recStream(stream, 'remoteVideo')
+        })
+    })
+
+    // 연결 끊기
+    $('#hangout').on('click', shutDown);
+}
--- myPractice/public/javascripts/users.js
+++ myPractice/public/javascripts/users.js
@@ -1,122 +1,144 @@
 'use strict'
+$(document).ready(whatIsRoomName)
 
-/////////////////////////////////////////////////////////////////
-////////////////////////// webRTC PART /////////////////////////
-///////////////////////////////////////////////////////////////
+$('.enter').on('click', joinRoom)
 
-let localStream;
-let remoteStream;
-
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-const constraints = {
-    video: true,
-    audio: false
-};
-
-// STUN server config
-const pcConfig = {
-    'iceServers': [
-        { 'urls': 'stun:stun.l.google.com:19302' },
-        { 'urls': 'stun:stun.services.mozilla.com' }
-    ]
-};
-
-// check if the browser supports the WebRTC
-function hasLocalMedia() {
-    return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+function whatIsRoomName() {
+    $('#localVideo').hide();
+    $('#room').hide();
 }
 
-// get media
-function recStream(stream, elemid) {
-    let video = document.getElementById(elemid);
+async function joinRoom() {
+    let peerId = await $('.roomId').val();
 
-    video.srcObject = stream;
-    remoteStream = stream;
-}
-
-// get local media
-function getLocalStream(stream) {
-    localStream = stream;
-    recStream(stream, 'localVideo');
-}
-
-// Handles error by logging a message to the console with the error message.
-function handleLocalMediaStreamError(error) {
-    console.log('navigator.getUserMedia error: ', error);
-}
-
-if (hasLocalMedia()) {
-
-    // enabling video and audio channels
-    navigator.mediaDevices.getUserMedia(constraints)
-        .then(getLocalStream)
-        .catch(handleLocalMediaStreamError)
-
-} else {
-    alert('webRTC is not supported!')
-}
+    $('#localVideo').show();
+    $('#room').show();
+    $('.title').hide();
+    $('.roomId').hide();
+    $('.enter').hide();
 
 
-/////////////////////////////////////////////////////////////////
-////////////////////////// peerJS PART /////////////////////////
-///////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////////
+    ////////////////////////// webRTC PART /////////////////////////
+    ///////////////////////////////////////////////////////////////
 
-let peer = new Peer({ key: 'lwjd5qra8257b9' });
+    let localStream;
+    let remoteStream;
 
-peer.on('open', (id) => {
-    document.getElementById("displayId").innerHTML = peer.id
-    console.log(id)
-})
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-// try to connect
-function connecting() {
-    let peerId = $('.roomId').val();
-    let conn = peer.connect(peerId, {label: 'room'});
-    
-    // room 으로 보내는 값 (2)
-    conn.on('open', () => {
-        conn.send(peer.id);
-    })
+    const constraints = {
+        video: true,
+        audio: false
+    };
 
-    // connect 시에 room 으로 부터 데이터 받는 곳 (3)
-    conn.on('data', (data) => {
-        console.log('1 ', data);
-    })
-}
+    // STUN server config
+    const pcConfig = {
+        'iceServers': [
+            { 'urls': 'stun:stun.l.google.com:19302' },
+            { 'urls': 'stun:stun.services.mozilla.com' }
+        ]
+    };
 
-// listen connecting
-peer.on('connection', (connection) => {
-    console.log('connection' , connection)
-    
-    // room 으로 부터 응답 올 때 받는 값 (1)
-    connection.on('data', (data) => {
-        if (data === "room is ready") {
-            navigator.getUserMedia(constraints, (stream) => {
-                var call = peer.call(connection.peer, stream);
-                call.on('stream', (stream) => {
-                    remoteStream = stream;
-                    recStream(remoteStream, 'remoteVideo')
-                })
-            })
-        } else {
-            alert('connecting error 1');
-        }
-    })
+    // check if the browser supports the WebRTC
+    function hasLocalMedia() {
+        return !!(navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+    }
 
-    // room 으로 부터 응답 올 때 주는 값 (4)
-    connection.on('open', () => {
-        connection.send('user is ready');
-    })
-})
+    // get media
+    function recStream(stream, elemid) {
+        let video = document.getElementById(elemid);
 
-// 전화 대기
-peer.on('call', (call) => {
-    call.answer(localStream);
-    call.on('stream', (stream) => {
+        video.srcObject = stream;
         remoteStream = stream;
-        recStream(stream, 'remoteVideo')
-    })
-})
+    }
 
-$('.enter').on('click', connecting)
+    // get local media
+    function getLocalStream(stream) {
+        localStream = stream;
+        recStream(stream, 'localVideo');
+    }
+
+    // Handles error by logging a message to the console with the error message.
+    function handleLocalMediaStreamError(error) {
+        console.log('navigator.getUserMedia error: ', error);
+    }
+
+    if (hasLocalMedia()) {
+
+        // enabling video and audio channels
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(getLocalStream)
+            .catch(handleLocalMediaStreamError)
+
+    } else {
+        alert('webRTC is not supported!')
+    }
+
+
+    /////////////////////////////////////////////////////////////////
+    ////////////////////////// peerJS PART /////////////////////////
+    ///////////////////////////////////////////////////////////////
+
+    let peer = await new Peer({ key: 'lwjd5qra8257b9' });
+
+    await peer.on('open', (id) => {
+        // document.getElementById("displayId").innerHTML = peer.id
+    })
+
+    // try to connect
+    function connecting() {
+        
+        let conn = peer.connect(peerId, { label: 'room' });
+        // room 으로 보내는 값 (1)
+        conn.on('open', () => {
+            // user id 전송
+            conn.send(peer.id);
+        })
+
+        // connect 시에 room 으로 부터 데이터 받는 곳 (2)
+        conn.on('data', (data) => {
+            if (data.would === '꺼져') {
+                alert('입장 거절당하셨습니당')
+            } else {
+                alert('입장합니다')
+                document.getElementById("displayId").innerHTML = data.name;
+            }
+        })
+    }
+
+    connecting();
+
+    // listen connecting
+    peer.on('connection', (connection) => {
+
+        // room 으로 부터 응답 올 때 받는 값 (3)
+        connection.on('data', (data) => {
+            if (data === "room is ready") {
+                navigator.getUserMedia(constraints, (stream) => {
+                    var call = peer.call(connection.peer, stream);
+                    call.on('stream', (stream) => {
+                        remoteStream = stream;
+                        recStream(remoteStream, 'remoteVideo')
+                    })
+                })
+            } else {
+                alert('connecting error 1');
+            }
+        })
+
+        // room 으로 부터 응답 올 때 주는 값 (4)
+        connection.on('open', () => {
+            connection.send('user is ready');
+        })
+    })
+
+    // 전화 대기
+    peer.on('call', (call) => {
+        call.answer(localStream);
+        call.on('stream', (stream) => {
+            remoteStream = stream;
+            recStream(stream, 'remoteVideo')
+        })
+    })
+}
